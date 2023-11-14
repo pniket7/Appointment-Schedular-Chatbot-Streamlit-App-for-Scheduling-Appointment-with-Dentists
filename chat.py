904ef89fff299c8bf51e3ef913d83f4dd59d0fc0
Niket Patil <pniket7@gmail.com>
2023-11-14 12:50:53 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -8,51 +8,42 @@ def main():
     # Load the OpenAI API key from Streamlit secrets
     openai.api_key = st.secrets["api_key"]
 
+    # Check if the sessionAdvisor object exists in session_state
+    if "sessionAdvisor" not in st.session_state:
+        # Initialize the AdvisorGPT if it doesn't exist in session_state
+        st.session_state.sessionAdvisor = ChatSession(gpt_name='Advisor')
+
+        # Instruct GPT to become a financial advisor.
+        st.session_state.sessionAdvisor.inject(
+            line="You are a financial advisor at a bank. Start the conversation by inquiring about the user's financial goals. If the user mentions a specific financial goal or issue, acknowledge it and offer to help. Be attentive to the user's needs and goals.",
+            role="user"
+        )
+        st.session_state.sessionAdvisor.inject(line="Ok.", role="assistant")
+
     # Create a Streamlit text area for user input with a unique key
     user_input = st.text_area("User:", key="user_input")
 
     # Create a Streamlit button with a unique key to send the user input
     if st.button("Send", key="send_button"):
-        if "sessionAdvisor" not in st.session_state:
-            # Initialize the AdvisorGPT if it doesn't exist in session_state
-            st.session_state.sessionAdvisor = ChatSession(gpt_name='Advisor')
-
-            # Instruct GPT to become a financial advisor.
-            st.session_state.sessionAdvisor.inject(
-                line="You are a financial advisor at a bank. Start the conversation by inquiring about the user's financial goals. If the user mentions a specific financial goal or issue, acknowledge it and offer to help. Be attentive to the user's needs and goals. ",
-                role="user"
-            )
-            st.session_state.sessionAdvisor.inject(line="Ok.", role="assistant")
-
         # Update the chat session with the user's input
         st.session_state.sessionAdvisor.chat(user_input=user_input, verbose=False)
 
-        # Get the chat history, which includes the chatbot's response
-        chat_history = st.session_state.sessionAdvisor.messages
+    # Get the chat history, which includes the chatbot's response
+    chat_history = st.session_state.sessionAdvisor.messages
 
-        # Extract the chatbot's response from the last message in the history
-        advisor_response = chat_history[-1]['content'] if chat_history else ""
+    # Extract the chatbot's response from the last message in the history
+    advisor_responses = [message['content'] for message in chat_history] if chat_history else []
 
-        # Display the chatbot's response with text wrapping
-        st.markdown(f'**Advisor:** {advisor_response}', unsafe_allow_html=True)
+    # Display the chat history with text wrapping
+    for response in advisor_responses:
+        st.markdown(f'**Advisor:** {response}', unsafe_allow_html=True)
 
     # Create a button to start a new conversation
     if st.button("New Chat", key="new_chat_button"):
-        if "sessionAdvisor" in st.session_state:
-            # Remove the existing sessionAdvisor from session_state
-            del st.session_state.sessionAdvisor
         st.text("New conversation started. You can now enter your query.")
-
-        # Clear the user input field by updating the user_input variable
-        user_input = ""
 
     # Create a button to exit the current conversation
     if st.button("Exit Chat", key="exit_chat_button"):
-        if "sessionAdvisor" in st.session_state:
-            # Remove the existing sessionAdvisor from session_state to exit the chat
-            del st.session_state.sessionAdvisor
-
-
         st.text("Chatbot session exited. You can start a new conversation by clicking the 'New Chat' button.")
 
 if __name__ == "__main__":
