fdb3f3c41acea80ad24a7dd0b6aa573829936dc7
Niket Patil <pniket7@gmail.com>
2023-11-14 15:46:56 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -6,53 +6,35 @@ def main():
     st.title('Financial Bank Advisor Chatbot')
 
     # Load the OpenAI API key from Streamlit secrets
-    openai.api_key = st.secrets["api_key"]
+    openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-    # Define CSS for the chat window
-    st.markdown("""
-    <style>
-    .chat-container {
-        background-color: #f5f5f5;
-        padding: 15px;
-        border-radius: 8px;
-        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-        max-width: 800px;
-        margin-bottom: 20px;
-    }
-    .user-msg {
-        background-color: #ddeeff;
-        border-radius: 5px;
-        padding: 8px;
-        margin-bottom: 5px;
-        width: 70%;
-        float: left;
-        color: #000; /* Black text for user messages */
-    }
-    .bot-msg {
-        background-color: #e6e6e6;
-        border-radius: 5px;
-        padding: 8px;
-        margin-bottom: 5px;
-        width: 70%;
-        float: right;
-        color: #000; /* Black text for bot messages */
-    }
-    </style>
-    """, unsafe_allow_html=True)
+    # Initialize chat history in session state if it doesn't exist
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = []
 
-    # Create a container for the chat window
-    chat_container = st.empty()
+    # Display chat messages from history on app rerun
+    for message in st.session_state.chat_history:
+        if message["role"] == "user":
+            st.chat_message(f"User: {message['content']}", side="right")
+        else:
+            st.chat_message(f"Bot: {message['content']}", side="left")
 
-    # Create a Streamlit text area for user input
-    user_input = st.text_area("User Input:")
+    # Accept user input
+    user_input = st.text_input("Type your message here...")
 
     # Create a button to send the user input
     if st.button("Send"):
+        # Add the user's message to the chat history
+        st.session_state.chat_history.append({"role": "user", "content": user_input})
+
+        # Update the chat session with the user's input
         if "sessionAdvisor" not in st.session_state:
             # Initialize the AdvisorGPT if it doesn't exist in session_state
             st.session_state.sessionAdvisor = ChatSession(gpt_name='Advisor')
+
+            # Instruct GPT to become a financial advisor.
             st.session_state.sessionAdvisor.inject(
-                line="You are a financial advisor at a bank. Start the conversation by inquiring about the user's financial goals. If the user mentions a specific financial goal or issue, acknowledge it and offer to help. Be attentive to the user's needs and goals.",
+                line="You are a financial advisor at a bank. Start the conversation by inquiring about the user's financial goals. If the user mentions a specific financial goal or issue, acknowledge it and offer to help. Be attentive to the user's needs and goals. ",
                 role="user"
             )
             st.session_state.sessionAdvisor.inject(line="Ok.", role="assistant")
@@ -60,31 +42,21 @@ def main():
         # Update the chat session with the user's input
         st.session_state.sessionAdvisor.chat(user_input=user_input, verbose=False)
 
-        # Get the updated chat history
-        chat_history = st.session_state.sessionAdvisor.messages
+        # Get the chatbot's response from the last message in the history
+        advisor_response = st.session_state.sessionAdvisor.messages[-1]['content'] if st.session_state.sessionAdvisor.messages else ""
 
-        # Display the chat window
-        with st.container():
-            for index, message in enumerate(chat_history):
-                role = message['role']
-                content = message['content']
-                if role == 'user':
-                    st.markdown(f'<div class="user-msg">{content}</div>', unsafe_allow_html=True)
-                elif role == 'assistant' and index > 0:  # Skip displaying the initial guidance message
-                    st.markdown(f'<div class="bot-msg">{content}</div>', unsafe_allow_html=True)
+        # Add the chatbot's response to the chat history
+        st.session_state.chat_history.append({"role": "bot", "content": advisor_response})
 
     # Create a button to start a new conversation
     if st.button("New Chat"):
-        if "sessionAdvisor" in st.session_state:
-            del st.session_state.sessionAdvisor
-        st.text("New conversation started. You can now enter your query.")
-        user_input = ""
+        # Clear the chat history to start a new conversation
+        st.session_state.chat_history = []
 
     # Create a button to exit the current conversation
     if st.button("Exit Chat"):
-        if "sessionAdvisor" in st.session_state:
-            del st.session_state.sessionAdvisor
-        st.text("Chatbot session exited. You can start a new conversation by clicking the 'New Chat' button.")
+        # Clear the chat history to exit the chat
+        st.session_state.chat_history = []
 
 if __name__ == "__main__":
     main()
