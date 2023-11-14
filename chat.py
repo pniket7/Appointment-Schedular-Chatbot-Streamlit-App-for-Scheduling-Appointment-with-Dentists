8b3e5ace57cec2a66a56029f6717d1eeace6b36c
Niket Patil <pniket7@gmail.com>
2023-11-14 16:53:13 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -21,15 +21,15 @@ def main():
         )
         st.session_state.sessionAdvisor.inject(line="Ok.", role="assistant")
 
-    # Display chat messages from history on app rerun
-    for message in st.session_state.chat_history:
-        if message["role"] == "user":
-            st.chat_message(message['content'], message['role'])
-        else:
-            st.chat_message(message['content'], message['role'], author_name='Bot')
+    # Display chat messages using st.chat
+    with st.expander("Chat"):
+        st.chat([
+            {"role": message["role"], "text": message["content"]} 
+            for message in st.session_state.chat_history
+        ])
 
     # Accept user input
-    user_input = st.chat_input("Type your message here...")
+    user_input = st.text_input("Type your message here...")
 
     # Create a button to send the user input
     if st.button("Send"):
@@ -39,36 +39,15 @@ def main():
         # Update the chat session with the user's input
         st.session_state.sessionAdvisor.chat(user_input=user_input, verbose=False)
 
-        # Get the chatbot's response from the last message in the history
-        advisor_response = st.session_state.sessionAdvisor.messages[-1]['content'] if st.session_state.sessionAdvisor.messages else ""
+        # Get the chatbot's response from OpenAI
+        advisor_response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=user_input,
+            max_tokens=50
+        ).choices[0].text
 
         # Add the chatbot's response to the chat history
         st.session_state.chat_history.append({"role": "bot", "content": advisor_response})
-
-        # Display the latest response
-        st.chat_message(advisor_response, 'Bot')
-
-    # Create a button to start a new conversation
-    if st.button("New Chat"):
-        # Clear the chat history to start a new conversation
-        st.session_state.chat_history = []
-
-        # Reinitialize sessionAdvisor for a new conversation
-        st.session_state.sessionAdvisor = ChatSession(gpt_name='Advisor')
-        st.session_state.sessionAdvisor.inject(
-            line="You are a financial advisor at a bank. Start the conversation by inquiring about the user's financial goals. If the user mentions a specific financial goal or issue, acknowledge it and offer to help. Be attentive to the user's needs and goals. ",
-            role="user"
-        )
-        st.session_state.sessionAdvisor.inject(line="Ok.", role="assistant")
-
-    # Create a button to exit the current conversation
-    if st.button("Exit Chat"):
-        # Clear the chat history to exit the chat
-        st.session_state.chat_history = []
-
-    # Display status messages
-    if "status" in st.session_state:
-        st.status(st.session_state.status)
 
 if __name__ == "__main__":
     main()
