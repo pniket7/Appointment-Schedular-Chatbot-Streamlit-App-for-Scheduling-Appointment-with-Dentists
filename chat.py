1818db3fafa31253b3f3ee4235250bf6ea9b47b1
Niket Patil <pniket7@gmail.com>
2023-11-15 12:08:49 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -85,5 +85,17 @@ def main():
         chat_container.markdown(f'<div style="border: 1px solid black; padding: 10px; height: 400px; overflow-y: scroll;">{chat_messages}</div>', unsafe_allow_html=True)
         st.markdown("Chatbot session exited. You can start a new conversation by clicking the 'New Chat' button.")
 
+    # Add custom CSS to change the background color of the user's chat box
+    st.markdown(
+        """
+        <style>
+        .stTextInput > div > div > div:nth-child(2) {
+            background-color: #e6e6e6 !important;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
 if __name__ == "__main__":
     main()
